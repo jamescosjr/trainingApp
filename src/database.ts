28db09a21374e35f7e907cf96d71ba629c09b388
James Junior <jamescosjr@gmail.com>
2024-04-01 03:18:45 -0300

    Organizando, colocando o import dotenv no server

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,4 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -9,14 +6,14 @@ if (!DATABASE_URL) {
   throw new Error("A variável de ambiente DATABASE_URL está indefinida.");
 }
 
-// Função para conectar ao banco de dados
+
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(DATABASE_URL);
     console.log('Connected to the database successfully');
   } catch (err: any) {
     console.error(`Error connecting to the database: ${err.message}`);
-    process.exit(1); // Encerra a aplicação se não conseguir conectar ao banco de dados
+    process.exit(1);
   }
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,26 @@
 import express from 'express';
-import connectToDatabase from './database'; // Ajuste o caminho de importação conforme necessário
+import connectToDatabase from './database';
 import trainingRoutes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-// Conectar ao banco de dados
+
 connectToDatabase()
   .then(() => {
     console.log('Database connection established');
-    // Definir as rotas após a conexão bem-sucedida
+    
     app.use('/api', trainingRoutes);
 
-    // Iniciar o servidor
+    
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => {
     console.error('Failed to connect to the database', error);
-    process.exit(1); // Encerra a aplicação se a conexão falhar
+    process.exit(1);
   });
